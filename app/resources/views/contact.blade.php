5bbb70b5c316d250151fcac3ed855c27c381c225
ユーザー名 <idakoutuu0205@gmail.com>
2020-08-12 13:31:47 +0900

    お問い合わせページ作成

---
+++ app/resources/views/contact.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.base')
+
+@section('content')
+<h1 class="site-heading text-center text-white d-none d-lg-block">
+    <span class="site-heading-upper text-primary mb-3">取り上げて欲しいニュースや時事ネタなどなんでも気軽にどうぞ！</span>
+    <span class="site-heading-lower">お問い合わせページ</span>
+</h1>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">おっくんの政治チャンネル</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="https://www.youtube.com/channel/UCPxnT20QYztJkGSmE1liRSA?view_as=subscriber">youtube</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="https://twitter.com/CQ5SXmczvr8BzkE">Twitter</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="">Blog</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <hr>
+  <form action="{{ route('check') }}" method="post" style="text-align: center;">
+    <p class="contact">件名</p>
+    <input type="text" name="title">
+    <p class="contact">本文</p>
+    <textarea name="text" cols="30" rows="10"></textarea>
+    <br>
+    <input type="submit" value="送信">
+  </form>
+  <hr>
+  <style>
+      .contact {
+          color: white;
+      }
+  </style>
+
+@endsection
