1f8387f4789cc48972bcc3a49b462ef3b99e1d65
ユーザー名 <idakoutuu0205@gmail.com>
2020-08-12 13:40:32 +0900

    お問い合わせページブレード変更

--- app/resources/views/contact.blade.php
+++ app/resources/views/contact.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.base')
 
+@section('title')
+お問い合わせ
+@endsection
+
 @section('content')
 <h1 class="site-heading text-center text-white d-none d-lg-block">
     <span class="site-heading-upper text-primary mb-3">取り上げて欲しいニュースや時事ネタなどなんでも気軽にどうぞ！</span>
@@ -9,14 +13,14 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
     <div class="container">
-      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">おっくんの政治チャンネル</a>
+     <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="{{ route('index') }}">おっくんの政治チャンネル</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav mx-auto">
           <li class="nav-item active px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="#">Home
+            <a class="nav-link text-uppercase text-expanded" href="{{ route('index') }}">Home
               <span class="sr-only">(current)</span>
             </a>
           </li>
