208c8d5cf4dfb1168f2e486505971b3f06f1dea0
ユーザー名 <idakoutuu0205@gmail.com>
2020-08-12 13:32:05 +0900

    index.blade.php変更

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -1,28 +1,6 @@
+@extends('layouts.base')
 
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>okkunch</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/business-casual.min.css" rel="stylesheet">
-
-</head>
-
+@section('content')
 <body>
 
   <h1 class="site-heading text-center text-white d-none d-lg-block">
@@ -141,7 +119,7 @@
             </h2>
             <p class="mb-0">取り上げて欲しいニュースや時事ネタなどメッセ―ジお待ちしております！！</p>
             <div class="intro-button mx-auto">
-            <a class="btn btn-primary btn-xl" href="#">Message form</a>
+            <a class="btn btn-primary btn-xl" href="{{ route('contact') }}">Message form</a>
           </div>
           </div>
         </div>
@@ -151,16 +129,4 @@
     
   </section>
 
-  <footer class="footer text-faded text-center py-5">
-    <div class="container">
-      <p class="m-0 small">Copyright &copy; Your Website 2020</p>
-    </div>
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
+  @endsection
