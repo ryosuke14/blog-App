90bdb50ec2400ab4054117397237719050eff3a2
ユーザー名 <idakoutuu0205@gmail.com>
2020-08-28 23:04:03 +0900

    お問い合わせテーブルidをincrementsに修正

--- app/database/migrations/2020_08_23_112253_create_contacts_table.php
+++ app/database/migrations/2020_08_23_112253_create_contacts_table.php
@@ -14,7 +14,7 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->string('text');
             $table->timestamps();
