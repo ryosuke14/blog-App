087918f88be66dfb3541e6d01419038dda7df358
ユーザー名 <idakoutuu0205@gmail.com>
2020-08-30 19:01:49 +0900

    お問い合わせページ修正

--- app/resources/views/contact.blade.php
+++ app/resources/views/contact.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <h1 class="site-heading text-center text-white d-none d-lg-block">
     <span class="site-heading-upper text-primary mb-3">取り上げて欲しいニュースや時事ネタなどなんでも気軽にどうぞ！</span>
-    <span class="site-heading-lower">お問い合わせページ</span>
+    <span class="site-heading-lower">お問い合わせ</span>
 </h1>
 
 <!-- Navigation -->
@@ -44,34 +44,37 @@
         <div class="row">
             <div class="col-12">
                 <p class="contact">件名</p>
-                <textarea name="title" cols="50" rows="1" value="">{{ old('title') }}</textarea>
+                <textarea name="title" cols="65" rows="1" value="">{{ old('title') }}</textarea>
                 @if ($errors->has('title'))
                 <p class="error-message">{{ $errors->first('title') }}</p>
                 @endif
             </div>
-           
             <div class="col-12">
                 <p class="contact">本文</p>
-                <textarea name="text" cols="50" rows="20" value="">{{ old('text') }}</textarea>
+                <textarea name="text" cols="65" rows="20" value="">{{ old('text') }}</textarea>
                 @if ($errors->has('text'))
                     <p class="error-message">{{ $errors->first('text') }}</p>
                 @endif
             </div>
             <br>
-            <div class="col-12">
+            <div class="col-12 confilm">
                 <input type="submit" value="確認する">
             </div>
         </div>
     </div>
   </form>
   <hr>
-  <style>
+  <style>    
       .contact {
           color: white;
       }
       
       .error-message {
         color: red;
+      }
+
+      .confilm {
+        margin-top: 20px;
       }
   </style>
 
