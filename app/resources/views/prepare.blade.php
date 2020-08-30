358b59878cb7d9a1978a8a366e2ec166d5f1d799
ユーザー名 <idakoutuu0205@gmail.com>
2020-08-30 19:02:22 +0900

    準備中ページ作成

---
+++ app/resources/views/prepare.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.base')
+
+@section('title')
+blog
+@endsection
+
+@section('content')
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="{{ route('index') }}">おっくんの政治チャンネル</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="{{ route('index') }}">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://www.youtube.com/channel/UCPxnT20QYztJkGSmE1liRSA?view_as=subscriber">youtube</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://twitter.com/CQ5SXmczvr8BzkE">Twitter</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="{{ route('blog') }}">Blog</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+        <h2 class="info">※ただいま準備中です※</h2>
+    </div>
+</body>
+<style>
+    .info {
+        font-size: 2rem;
+        text-align: center;
+        color: red;
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+</style>
+  
+@endsection
