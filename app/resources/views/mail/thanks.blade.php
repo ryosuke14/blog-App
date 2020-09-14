c631cfa010aeed2715925c3f97194b18f794493e
ryosuke14 <ykdyd2kut@gmail.com>
2020-09-14 23:13:24 +0900

    メール完成

--- app/resources/views/mail/thanks.blade.php
+++ app/resources/views/mail/thanks.blade.php
@@ -1,1 +1,48 @@
-<p>テストメールの送信です。</p>
+
+<!DOCTYPE html>
+<html lang="ja">
+<style>
+  body {
+    background-color: #fffacd;
+  }
+  h1 {
+    font-size: 13px;
+    color: #ff6666;
+  }
+  #button {
+    width: 150px;
+    text-align: center;
+  }
+  #button a {
+    padding: 10px 20px;
+    display: block;
+    border: 1px solid #2a88bd;
+    background-color: #FFFFFF;
+    color: #2a88bd;
+    text-decoration: none;
+    box-shadow: 2px 2px 3px #f5deb3;
+  }
+  #button a:hover {
+    background-color: #2a88bd;
+    color: #FFFFFF;
+  }
+</style>
+<body>
+<p></p>
+
+<p>お問合せありがとうございます。</p>
+<p>以下の内容でお問合せを受け付けました。</p>
+<div> </div>
+
+<p id="button">
+  <a href="https://www.youtube.com/channel/UCPxnT20QYztJkGSmE1liRSA?view_as=subscriber">おッくんチャンネル　YouTubeへ</a>
+</p>
+<p id="button">
+  <a href="https://twitter.com/CQ5SXmczvr8BzkE">おッくんチャンネル　Twitterへ</a>
+</p>
+<p id="button">
+  <a href="">おッくんチャンネル　ブログへ</a>
+</p>
+
+</body>
+</html>
